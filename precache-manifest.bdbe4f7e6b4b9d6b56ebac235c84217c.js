620709f09b906ba53481a73cf1e7101f2990e451
Beto Quiroga <beto.m12@gmail.com>
2019-07-31 12:43:37 -0400

    Updates

---
+++ precache-manifest.bdbe4f7e6b4b9d6b56ebac235c84217c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6865f03f3a4fbd54e9a591678b894aa7",
+    "url": "/wgewg/index.html"
+  },
+  {
+    "revision": "b76c702c13b6c826e6a2",
+    "url": "/wgewg/static/css/main.8a4643fa.chunk.css"
+  },
+  {
+    "revision": "a1b4ffa07d7c3b4977d6",
+    "url": "/wgewg/static/js/2.7d56673c.chunk.js"
+  },
+  {
+    "revision": "b76c702c13b6c826e6a2",
+    "url": "/wgewg/static/js/main.8ff48dec.chunk.js"
+  },
+  {
+    "revision": "b6cd705ece27736ab7e2",
+    "url": "/wgewg/static/js/runtime~main.b9a4c8dc.js"
+  }
+]);
